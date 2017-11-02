a9797e1a67b53f257c08a5da199f431cb1d35b2b
jeong-eui-jin <hehar1020@gmail.com>
2017-11-02 15:55:33 +0900

    view에 detail, result, vote추가 , urls에 경로 추가

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from polls.models import Question
+
 
 def index(request):
-    return HttpResponse('hello world.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("you're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "you're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("you're voting on question %s" % question_id)
